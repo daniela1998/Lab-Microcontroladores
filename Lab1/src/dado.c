f0ef19f1b023e06af64fc3826a4195b1525f5aa0
Danny <dannyrios@DESKTOP-OA8MO01>
2023-09-01 16:19:57 -0600

    Se agrega el makefile y se genera la funcion de delay en dado.c

--- Lab1/src/dado.c
+++ Lab1/src/dado.c
@@ -1,2 +1,17 @@
+#include <pic14/pic12f675.h>
+
 typedef unsigned int word;
 word __at 0x2007 __CONFIG = (_WDTE_OFF & _WDT_OFF & _MCLRE_OFF);
+
+
+
+
+// Funcion necesaria para el delay
+void delay(unsigned int tiempo)
+{
+	unsigned int i;
+	unsigned int j;
+
+	for(i=0;i<tiempo;i++)
+	  for(j=0;j<1275;j++);
+}
