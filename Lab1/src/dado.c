8293743331e18a0934be1cb2a0091bd5e882ff61
Danny <dannyrios@DESKTOP-OA8MO01>
2023-09-01 16:51:20 -0600

    Se empieza la logica

--- Lab1/src/dado.c
+++ Lab1/src/dado.c
@@ -3,6 +3,46 @@
 typedef unsigned int word;
 word __at 0x2007 __CONFIG = (_WDTE_OFF & _WDT_OFF & _MCLRE_OFF);
 
+// Se definen las funciones a utilizar para el delay y el rand_num
+void delay (unsigned tiempo);
+unsigned int rand(unsigned int min_value, unsigned int max_value);
+
+void main(void)
+{
+    // Todos los pines son salidas menos GP3
+    TRISIO = 0b00000000;
+    // Pines se ponen en bajo
+    GPIO = 0x00;
+    // Valor inicial del num rand
+    int rand_num =0b101;
+    // Cambia el valor del rand
+    int new = 0b000;
+    // Inicializa el tiempo 
+    unsigned int time = 100;
+    //
+
+ 
+    //Siempre se corre
+    while ( 1 )
+    {
+        // Se generan los numeros aleatorios con linear-feedback shift register method 
+		newbit=(rand_num ^ (rand_num >> 1)) & 1;
+		rand_num= (rand_num >> 1) | (new << 2);
+        // Si se presiona el boton 
+		if (GP3)
+		{
+            if (rand_num==0b001){
+
+                GPIO = 0b00000010;
+                delay(time);
+                GPIO = 0b0000000;
+            }
+            
+
+        }
+
+}
+}
 
 
 
